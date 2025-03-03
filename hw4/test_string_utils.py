0028e401662ce652764322433775a31506015851
nkeuschbaker <nkeuschbaker@friendsseminary.org>
2025-03-03 14:26:43 +0000

    wrote test_string_utils

--- hw4/test_string_utils.py
+++ hw4/test_string_utils.py
@@ -1,5 +1,14 @@
 import unittest
-class TestStringUtils(unittest.testcase):
+
+from string_utils import reverse_string, capitalize_string, is_capitalized
+class TestStringUtils(unittest.TestCase):
   def test_reverse_string(self):
     result = "olleh"
     self.assertEqual(result, reverse_string("hello"))
+  def test_capitalize_string(s):
+    result = "Hello"
+    s.assertEqual(result, capitalize_string("hello"))
+  def test_is_capitalized(s):
+    s.assertTrue(is_capitalized("Hello"))
+if __name__ == '__main__':
+  unittest.main()
